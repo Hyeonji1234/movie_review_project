803046bcb289f7e7a099fb614ea8095753f3d297
Hyeonji1234 <12kt@naver.com>
2025-12-13 03:47:15 +0900

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const api = axios.create({
+    baseURL: `${process.env.REACT_APP_API_BASE_URL}/api`,
+});
+
 
 // Axios 인스턴스 생성
 const api = axios.create({
